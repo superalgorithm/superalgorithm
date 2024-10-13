48720a6aa04cbdfee8bbbb721ee99b6875725ca9
arauser <alexander.rauser@gmail.com>
2024-10-13 10:07:43 +0400

    crash when no config.yaml is present #52

--- src/superalgorithm/utils/config.py
+++ src/superalgorithm/utils/config.py
@@ -1,5 +1,6 @@
 import os
 from pathlib import Path
+import warnings
 import yaml
 
 
@@ -10,16 +11,17 @@ def read_config(yaml_file=None):
     config = {}
     try:
         yaml_file = find_config() if yaml_file is None else yaml_file
+        # Try to read from YAML file if provided
+        if yaml_file and os.path.exists(yaml_file):
+            with open(yaml_file, "r") as file:
+                try:
+                    config = yaml.safe_load(file)
+                except yaml.YAMLError as e:
+                    raise e
     except Exception as e:
-        raise e
-
-    # Try to read from YAML file if provided
-    if yaml_file and os.path.exists(yaml_file):
-        with open(yaml_file, "r") as file:
-            try:
-                config = yaml.safe_load(file)
-            except yaml.YAMLError as e:
-                raise e
+        warnings.warn(
+            f"Could not find config.yaml or the file is damaged, proceeding with environment variables only. Error: {e}"
+        )
 
     # Read from environment variables
     for key, value in os.environ.items():
