ad9115a70c8962f26c7dc6b5f105871373108955
arauser <alexander.rauser@gmail.com>
2025-01-24 08:20:52 +0700

    Add hedge mode support in order creation for WOOExchange

--- src/superalgorithm/exchange/woo_exchange.py
+++ src/superalgorithm/exchange/woo_exchange.py
@@ -4,24 +4,25 @@ from superalgorithm.types.data_types import Order
 
 
 class WOOExchange(CCXTExchange):
-
     def __init__(self, config: Any = {}):
         super().__init__("woo", config)
 
+        self.hedge_mode = config.get("hedge_mode", False)
+
+    def _create_order_params(self, order: Order) -> dict:
+        params = {
+            "client_order_id": order.client_order_id,
+        }
+        if self.hedge_mode:
+            params["position_side"] = order.position_type.value
+        return params
+
     async def _create_limit_order(self, order: Order) -> str:
         return await super()._create_limit_order(
-            order,
-            {
-                "client_order_id": order.client_order_id,
-                "position_side": order.position_type.value,
-            },
+            order, self._create_order_params(order)
         )
 
     async def _create_market_order(self, order: Order) -> str:
         return await super()._create_market_order(
-            order,
-            {
-                "client_order_id": order.client_order_id,
-                "position_side": order.position_type.value,
-            },
+            order, self._create_order_params(order)
         )
